565c9d7215d5c30b3d78eb5e6a63f7da01fab941
gamerdonkey <gamerdonkey@gmail.com>
2024-07-23 04:52:31 +0000

    Changing execution so the reposter can run as a daemonized service.

--- masto2bsky.py
+++ masto2bsky.py
@@ -1,9 +1,11 @@
 import logging
 import requests
+import signal
 
-from atproto import Client as BlueskyClient, client_utils as bluesky_client_utils
+from atproto import (Client as BlueskyClient, client_utils as bluesky_utils, SessionEvent as BlueskySessionEvent)
 from bs4 import BeautifulSoup
 from mastodon import Mastodon
+from threading import Event
 
 
 logger = logging.getLogger(__name__)
@@ -15,6 +17,8 @@ class Masto2Bsky:
     MASTODON_TOKEN_FILENAME = "mastodon_token.secret"
 
     def __init__(self):
+        self.exit_event = Event()
+
         try:
             with open(self.LAST_STATUS_FILENAME, "r") as last_status_file:
                 self.last_status_id = int(last_status_file.read())
@@ -22,31 +26,53 @@ class Masto2Bsky:
         except FileNotFoundError:
             self.last_status_id = None
             
-        self.bluesky_client = BlueskyClient()
+        self.bluesky = BlueskyClient()
+        self.bluesky.on_session_change(self.on_bluesky_session_change)
         try:
             with open(self.BLUESKY_SESSION_FILENAME, "r") as bluesky_session_file:
-                self.bluesky_client.login(session_string=bluesky_session_file.read())
+                self.bluesky.login(session_string=bluesky_session_file.read())
 
         except FileNotFoundError as file_not_found_error:
             e = Exception("Could not find saved Bluesky session. Run 'save_bluesky_session.py'.")
             raise e from file_not_found_error
 
         self.mastodon = Mastodon(access_token=self.MASTODON_TOKEN_FILENAME)
+        self.mastodon_account = self.mastodon.me()
+
+    def on_bluesky_session_change(self, event, session):
+        if event in (BlueskySessionEvent.CREATE, BlueskySessionEvent.REFRESH):
+            self.save_bluesky_session(session.export())
 
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
+        self.save_bluesky_session(self.bluesky.export_session_string())
+        self.save_last_status()
+
+    def save_bluesky_session(self, bluesky_session):
         with open(self.BLUESKY_SESSION_FILENAME, "w") as bluesky_session_file:
-            bluesky_session = self.bluesky_client.export_session_string()
             bluesky_session_file.write(bluesky_session)
 
+    def save_last_status(self):
         with open(self.LAST_STATUS_FILENAME, "w") as last_status_file:
             last_status_file.write(str(self.last_status_id))
 
+    def on_sigint(self, signum, frame):
+        logger.warning(f"Exiting on signal: {signal.strsignal(signum)}")
+        self.exit_event.set()
+
+    def run(self):
+        self.exit_event.clear()
+        signal.signal(signal.SIGINT, self.on_sigint)
+        signal.signal(signal.SIGTERM, self.on_sigint)
+
+        while not self.exit_event.is_set():
+            self.process_feed()
+            self.exit_event.wait(timeout=60)
+
     def process_feed(self):
-        mastodon_account = self.mastodon.me()
-        statuses = self.mastodon.account_statuses(mastodon_account,
+        statuses = self.mastodon.account_statuses(self.mastodon_account,
                                                   exclude_reblogs=True,
                                                   since_id=self.last_status_id)
 
@@ -56,7 +82,7 @@ class Masto2Bsky:
             for status in statuses[::-1]:
                 if status.visibility == "public" \
                         and (status.in_reply_to_account_id is None \
-                            or status.in_reply_to_account_id == mastodon_account.id):
+                            or status.in_reply_to_account_id == self.mastodon_account.id):
                     logger.info(f"Resposting {status.url}")
                     self.post_to_bluesky(status)
 
@@ -79,10 +105,10 @@ class Masto2Bsky:
                     image_alts.append(media.description)
 
             if images:
-                self.bluesky_client.send_images(text=status_text, images=images, image_alts=image_alts)
+                self.bluesky.send_images(text=status_text, images=images, image_alts=image_alts)
 
         else:
-            self.bluesky_client.send_post(status_text)
+            self.bluesky.send_post(status_text)
 
     @staticmethod
     def parse_status(status):
@@ -97,7 +123,7 @@ class Masto2Bsky:
 
         fulltext = "\n\n".join(paragraphs)
 
-        text_builder = bluesky_client_utils.TextBuilder()
+        text_builder = bluesky_utils.TextBuilder()
         if len(fulltext) > 300:
             final_text = text_builder.text(f"{fulltext[:285]}... ").link("[Full Text]", status.url)
         else:
@@ -106,6 +132,10 @@ class Masto2Bsky:
         return final_text 
 
 
-if __name__ == "__main__":
+def main():
     with Masto2Bsky() as masto2bsky:
-        masto2bsky.process_feed()
+        masto2bsky.run()
+
+
+if __name__ == "__main__":
+    main()
