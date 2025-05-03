40234ffb38fe2be683c96efbf19ddbabff6f299f
gamerdonkey <gamerdonkey@gmail.com>
2025-05-03 04:47:33 +0000

    Making use of my new post splitter, that takes long Mastodon toots and generates a Bluesky thread rather than cutting it off.

--- masto2bsky/reposter.py
+++ masto2bsky/reposter.py
@@ -4,6 +4,7 @@ import signal
 
 from atproto import (Client as BlueskyClient, client_utils as bluesky_utils,
         SessionEvent as BlueskySessionEvent, models as bluesky_models)
+from masto2bsky.post_splitter import PostSplitter
 from masto2bsky.toot_parser import TootParser
 from mastodon import Mastodon
 from mastodon.errors import MastodonNetworkError
@@ -82,41 +83,44 @@ class Reposter:
                 self._last_toot_id = toot.id
 
     def post_to_bluesky(self, toot):
-        toot_text = TootParser(toot).text_builder
+        posts = PostSplitter(TootParser(toot).text_builder).split()
 
         reply_ref = None
-        if toot.in_reply_to_id \
-                and toot.in_reply_to_id == self._last_reposted_toot_id \
-                and self._last_post_ref \
-                and self._last_root_post_ref:
-            reply_ref = bluesky_models.AppBskyFeedPost.ReplyRef(parent=self._last_post_ref,
-                                                                root=self._last_root_post_ref)
+        in_thread = (toot.in_reply_to_id and toot.in_reply_to_id == self._last_reposted_toot_id)
+        media_posted = False
+        for post in posts:
+            if in_thread and self._last_post_ref and self._last_root_post_ref:
+                reply_ref = bluesky_models.AppBskyFeedPost.ReplyRef(parent=self._last_post_ref,
+                                                                    root=self._last_root_post_ref)
 
-        if toot.media_attachments:
-            images = []
-            image_alts = []
+            if toot.media_attachments and not media_posted:
+                media_posted = True
+                images = []
+                image_alts = []
 
-            for media in toot.media_attachments:
-                if media.type == "image":
-                    logger.info(f"Getting image {media.preview_url}")
-                    image_resp = requests.get(media.preview_url, stream=True)
-                    image_resp.raw.decode_content = True
-                    images.append(image_resp.raw.read())
+                for media in toot.media_attachments:
+                    if media.type == "image":
+                        logger.info(f"Getting image {media.preview_url}")
+                        image_resp = requests.get(media.preview_url, stream=True)
+                        image_resp.raw.decode_content = True
+                        images.append(image_resp.raw.read())
 
-                    image_alts.append(media.description)
+                        image_alts.append(media.description)
 
-            if images:
-                response = self._bluesky.send_images(text=toot_text,
-                                                     images=images,
-                                                     image_alts=image_alts,
-                                                     reply_to=reply_ref)
+                if images:
+                    response = self._bluesky.send_images(text=post.text,
+                                                         facets=post.facets,
+                                                         images=images,
+                                                         image_alts=image_alts,
+                                                         reply_to=reply_ref)
 
-        else:
-            response = self._bluesky.send_post(toot_text, reply_to=reply_ref)
+            else:
+                response = self._bluesky.send_post(text=post.text, facets=post.facets, reply_to=reply_ref)
+
+            in_thread = True
+            self._last_post_ref = bluesky_models.create_strong_ref(response)
+            if reply_ref is None:
+                self._last_root_post_ref = self._last_post_ref
 
         self._last_reposted_toot_id = toot.id
-        self._last_post_ref = bluesky_models.create_strong_ref(response)
-
-        if reply_ref is None:
-            self._last_root_post_ref = self._last_post_ref
 
--- masto2bsky/toot_parser.py
+++ masto2bsky/toot_parser.py
@@ -5,14 +5,10 @@ from atproto import client_utils, IdResolver
 
 
 class TootParser:
-    BLUESKY_LIMIT = 300
-    OVERRUN_MESSAGE_LENGTH = len("... [Full Text]")
 
     def __init__(self, toot):
-        self.num_chars_left = self.BLUESKY_LIMIT - self.OVERRUN_MESSAGE_LENGTH
         self._text_builder = client_utils.TextBuilder()
         self._resolver = IdResolver()
-        self.overrun = False
 
         self._parse(toot)
 
@@ -27,12 +23,10 @@ class TootParser:
             br.replace_with("\n")
 
         for tag in soup.descendants:
-            is_last_tag = (tag.next_element is None)
-
             if tag.name is None and tag.parent.name == 'p':
-                text = self._fit_text(tag.get_text(), is_last_tag)
-
+                text = tag.get_text()
                 last_end = 0
+
                 for m in re.finditer(r"\@[\w\-\.]+\w", text):
                     self._text_builder.text(text[last_end:m.start()])
 
@@ -50,14 +44,14 @@ class TootParser:
 
             elif tag.name == 'a':
                 if "hashtag" in tag.get('class', []):
-                    hashtag = self._fit_text(f"#{tag.span.get_text()}", is_last_tag)
+                    hashtag = f"#{tag.span.get_text()}"
 
                     self._text_builder.tag(hashtag, hashtag.lstrip("#"))
 
                 elif "class" not in tag.attrs:
                     url = tag['href']
                     visible_elements = [e.get_text() for e in tag.select(":not(.invisible)")]
-                    text = self._fit_text("".join(visible_elements), is_last_tag)
+                    text = "".join(visible_elements)
 
                     self._text_builder.link(text, url)
 
@@ -66,31 +60,11 @@ class TootParser:
                 instance = url.split('/')[2]
                 username = tag.a.span.get_text()
 
-                handle = self._fit_text(f"@{username}@{instance}", is_last_tag)
+                handle = f"@{username}@{instance}"
 
                 self._text_builder.link(handle, url)
 
             elif tag.name == 'p' \
-                    and tag.previous_sibling is not None \
-                    and self.num_chars_left >= 2:
+                    and tag.previous_sibling is not None:
                 self._text_builder.text("\n\n")
-                self.num_chars_left -= 2
-
-            if self.num_chars_left <= 0:
-                if self.overrun:
-                    self._text_builder.text("... ").link("[Full Text]", toot.url)
-                break
-
-    def _fit_text(self, text, is_last_tag):
-        return_value = text
-        length = len(text)
-
-        if length >= self.num_chars_left \
-                and not (is_last_tag and length <= (self.num_chars_left + self.OVERRUN_MESSAGE_LENGTH)):
-            return_value = text[:self.num_chars_left]
-            self.overrun = True
-
-        self.num_chars_left -= len(return_value)
-
-        return return_value
 
